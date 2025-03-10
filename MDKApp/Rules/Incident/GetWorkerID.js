67597c4f1971b364d3462b2490608973183081c4
Emre Cevik <emre.cevik@sap.com>
2025-03-10 12:01:27 +0100

    modified MDK App

--- MDKApp/Rules/Incident/GetWorkerID.js
+++ MDKApp/Rules/Incident/GetWorkerID.js
@@ -1,7 +1,5 @@
 export default function GetWorkerID(context) {
     let workerID = context.getGlobalDefinition('/MDKApp/Globals/Application/WorkerID.global').getValue();
-    alert("Hello World:" + workerID)
-
     let queryOptions = `$filter=serviceWorker_ID eq ${workerID}`;
 
     return queryOptions;
