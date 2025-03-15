71be44b105377c7a7c373fef1eee01eee2b207be
Emre Cevik <emre.cevik@sap.com>
2025-03-15 12:58:55 +0100

    replace old scheme app with new scheme

--- MDKApp/Rules/Incident/GetWorkerID.js
+++
@@ -1,6 +0,0 @@
-export default function GetWorkerID(context) {
-    let workerID = context.getGlobalDefinition('/MDKApp/Globals/Application/WorkerID.global').getValue();
-    let queryOptions = `$filter=serviceWorker_ID eq ${workerID}`;
-
-    return queryOptions;
-}
--- MDKAppNewScheme/Rules/Customer/Customer_FormatAddress.js
+++
@@ -1,8 +0,0 @@
-/**
- * Describe this function...
- * @param {IClientAPI} clientAPI
- */
-export default function Customer_FormatAddress(clientAPI) {
-	let cust = clientAPI.binding.customer;
-	return cust.AddressHouseNumber + ' ' + cust.AddressStreet + '\n' + cust.AddressCity + '\n' + cust.AddressCountry;
-}
